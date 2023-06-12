9943271a1fc6fc156804d8be1c0a29589650f091
Anas Nadeem <anas5678go@gmail.com>
2023-06-12 13:51:43 +0530

    Responsive sidebar is done

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,10 +1,14 @@
 // Sidenavbar trigger function
 let opensideNavbarTrigger = document.getElementById('opensideNavbarTrigger');
+let closesideNavbarTrigger = document.querySelector('.sideNavbarCloseIcon');
 let sideNavbar = document.getElementById('sideNavbar');
 // let closeSideNavbarTrigger = document.getElementById('closeSideNavbarTrigger');
 
 opensideNavbarTrigger.addEventListener('click', function () {
     sideNavbar.classList.toggle('sideNavbarActive');
+});
+closesideNavbarTrigger.addEventListener('click', function () {
+    sideNavbar.classList.remove('sideNavbarActive');
 });
 
 // closeSideNavbarTrigger.addEventListener('click', function () {
