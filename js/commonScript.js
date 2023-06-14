2bee87707330c623c24f6321dd0c86423c0621e1
Anas Nadeem <anas5678go@gmail.com>
2023-06-14 16:34:14 +0530

    scroll btn feature added

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,3 +1,21 @@
+// Top Func 
+const scrollBtn = document.getElementById('scrollBtn');
+
+window.onscroll = function () { scrollFunction() }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+scrollBtn.addEventListener('click', function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0
+})
+
 // Sidenavbar trigger function
 let opensideNavbarTrigger = document.getElementById('opensideNavbarTrigger');
 let closesideNavbarTrigger = document.querySelector('.sideNavbarCloseIcon');
